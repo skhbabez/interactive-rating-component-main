c1a39b68bb22fdda008284bf5644a8df42c5e559
Sascha Khbabez <sascha.khbabez@outlook.de>
2025-08-15 18:45:34 +0200

    Added templates and js logic

---
+++ main.js
@@ -0,0 +1,18 @@
+const ratingTemplate = document.getElementById("rating-card").content;
+const responseTemplate = document.getElementById("response-card").content;
+const ratingCard = ratingTemplate.cloneNode(true);
+const main = document.body.querySelector("main");
+const form = ratingCard.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const data = new FormData(event.currentTarget);
+  const rating = data.get("rating");
+  const responseCard = responseTemplate.cloneNode(true);
+  const score = responseCard.querySelector("p:first-of-type span");
+  score.textContent = rating;
+  main.innerHTML = "";
+  main.appendChild(responseCard);
+});
+
+document.body.querySelector("main").appendChild(ratingCard);
